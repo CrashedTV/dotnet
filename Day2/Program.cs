b600de2ef6314ad39b424737758879c814f2339c
CrashedTV <ayush.xtha01@gmail.com>
2026-04-10 12:23:48 +0545

    First Commit

---
+++ Day2/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+//identifiers
+// var name = "ABCD";
+var _name = "ABCDE";
+var name123 = "ABCDF";
+
+// data types
+int age = 25;
+string address = "abcde";
+bool isStudent = true;
+double price2 = 120.4;
+
+
+//variables
+
+
+
+// contant -> compile time constant 
+const double PI = 3.14; 
+//PI = 3.14; this doesnt change
+
+//readonly -> runtime constant
+// readonly double gravity = 9.8;
+
+
+//operators
+
+
+//PascalCase --> class, method, namespace
+
+//camelCase --> variable, parameter
+
+//snake_case
+
+
+
+Console.Write("Name: ");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
+
