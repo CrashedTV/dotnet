22d90bf45fa7cbd3f03ed96b04c7770153214b50
CrashedTV <ayush.xtha01@gmail.com>
2026-04-19 07:16:10 +0545

    Collection Day2

---
+++ Day7/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+    1. Collection
+        a. Arrays
+        b. Lists
+        c. Dictionaries
+        d. HashSets
+        e. Queue
+        f. Stack
+
+    2. LINQ
+    3. File Operations
+
+*/
+// 1.a. Array  -> A collection of items stored at contiguous memory locations. It allows you to store multiple values of the same type in a single variable. 
+//             -> Arrays have a fixed size, meaning you need to specify the number of elements it can hold when you create it.
+// Types of Array
+// 1. Single-Dimensional Array -> A single-dimensional array is a linear collection of elements that are accessed using a single index. It is the most basic type of array and is often referred to as a "one-dimensional array". Each element in the array can be accessed using its index, which starts at 0. For example, if you have an array of integers with 5 elements, you can access the first element using index 0, the second element using index 1, and so on up to index 4 for the last element. Single-dimensional arrays are commonly used for storing lists of items, such as names, numbers, or any other type of data that can be represented in a linear format.
+
+// 2. Multi-Dimensional Array -> A 
+// 3. Jagged Array
